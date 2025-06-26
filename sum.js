47d17ad2f8c5da91fe1eeb784c53052be4d1dfad
NhatHM4 <haminhnhat711@gmail.com>
2025-06-26 22:33:21 +0700

    z

--- sum.js
+++ sum.js
@@ -1,10 +1,6 @@
 function sum(a, b) {
-    if (a === b) {
-        // If both numbers are equal, return their sum
-        return a + b;
-    }
-    // If numbers are not equal, return their difference
-    return a - b;
+    // If numbers are not equal, return their sum
+    return a + b;
 }
 
 module.exports = sum;
