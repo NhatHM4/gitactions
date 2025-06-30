22f206ef4196a659b7ebfa5621377f614f563148
NhatHM4 <haminhnhat711@gmail.com>
2025-06-30 08:42:53 +0700

    nmnmnm

--- sum.js
+++ sum.js
@@ -1,9 +1,6 @@
 function sum(a, b) {
     // If numbers are not equal, return their sum ahihi
-    if (a != b) {
         return a + b;
-    }
-    return a - b; // If numbers are equal, return their difference
 }
 
 
--- sum.test.js
+++ sum.test.js
@@ -4,8 +4,5 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 2 + 2 to equal 0', () => {
-  expect(sum(2, 2)).toBe(0);
-});
 
 
